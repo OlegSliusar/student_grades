236e52289600ebf69ae7189b70cf6edb73516a6f
Oleh Sliusar <osliustc@training.local>
2019-01-29 15:46:20 +0200

    Make migrations

--- my_site/models.py
+++ my_site/models.py
@@ -1,13 +1,15 @@
+from django.conf import settings
 from django.db import models
+# from django.contrib.auth.models import User
 
-class User:
+
+class User(models.Model):
     class Meta:
         db_table = "user"
 
-    Name = models.CharField(max_length=60, default=None)
-    Surname = models.CharField(max_length=60, default=None)
+    name = models.CharField(max_length=60, default=None)
+    surname = models.CharField(max_length=60, default=None)
     fRole = models.ForeignKey('Role', on_delete=models.DO_NOTHING)
-    Department = models.ManyToManyField('Department')
 
 class Role(models.Model):
     class Meta:
@@ -32,4 +34,28 @@ class Permission(models.Model):
 class Step(models.Model):
     class Meta:
         db_table = "step"
-    title = models.CharField
+    title = models.CharField(max_length=35)
+    fSection = models.ForeignKey('Section', on_delete=models.DO_NOTHING)
+
+class Section(models.Model):
+    class Meta:
+        db_table = "section"
+    name = models.CharField(max_length=35)
+
+class Answer(models.Model):
+    class Meta:
+        db_table = "answer"
+
+    fUser = models.ForeignKey("User", default=None, on_delete=models.CASCADE)
+
+    fQuestion = models.ForeignKey("Question", on_delete=models.DO_NOTHING)
+    answer_like = models.BooleanField()
+    # fExpert = models.ForeignKey("User", on_delete=models.CASCADE, related_name='fExpert')
+    fGrade = models.ForeignKey('Grade', on_delete=models.DO_NOTHING)
+
+class Question(models.Model):
+    class Meta:
+        db_table = "question"
+    title = models.CharField(max_length=35)
+    fStep = models.ForeignKey("Step", on_delete=models.DO_NOTHING)
+

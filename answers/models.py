894af915a665789be2dc32bde1c854137476667e
Oleg Sliusar <olehsiusar@gmail.com>
2019-01-30 17:26:39 +0200

    Move models

--- answers/models.py
+++ answers/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Answer(models.Model):
+    class Meta:
+        db_table = "answer"
+
+    fUser = models.ForeignKey("users.User", default=None, on_delete=models.CASCADE)
+
+    fQuestion = models.ForeignKey("my_site.Question", on_delete=models.DO_NOTHING)
+    answer_like = models.BooleanField()
+    fGrade = models.ForeignKey('my_site.Grade', on_delete=models.DO_NOTHING)
+
+class Review(models.Model):
+    class Meta:
+        db_table = "review"
+    fAnswer = models.ForeignKey("Answer", on_delete=models.DO_NOTHING)
+    fGrade = models.ForeignKey("my_site.Grade", on_delete=models.DO_NOTHING)
+    comment = models.TextField()
+    fUserExpert = models.ForeignKey("users.User", default=None, on_delete=models.CASCADE)
+
--- my_site/models.py
+++ my_site/models.py
@@ -34,26 +34,8 @@ class Section(models.Model):
         db_table = "section"
     name = models.CharField(max_length=35)
 
-class Answer(models.Model):
-    class Meta:
-        db_table = "answer"
-
-    fUser = models.ForeignKey("users.User", default=None, on_delete=models.CASCADE)
-
-    fQuestion = models.ForeignKey("Question", on_delete=models.DO_NOTHING)
-    answer_like = models.BooleanField()
-    fGrade = models.ForeignKey('Grade', on_delete=models.DO_NOTHING)
-
 class Question(models.Model):
     class Meta:
         db_table = "question"
     title = models.CharField(max_length=35)
     fStep = models.ForeignKey("Step", on_delete=models.DO_NOTHING)
-
-class Review(models.Model):
-    class Meta:
-        db_table = "review"
-    fAnswer = models.ForeignKey("Answer", on_delete=models.DO_NOTHING)
-    fGrade = models.ForeignKey("Grade", on_delete=models.DO_NOTHING)
-    comment = models.TextField()
-    fUserExpert = models.ForeignKey("users.User", default=None, on_delete=models.CASCADE)

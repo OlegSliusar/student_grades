c2c1244106c0074964bb0f61d28b97d1cb8f93cd
Oleg Sliusar <olehsiusar@gmail.com>
2019-02-04 21:24:38 +0200

    Notworking project

--- allactions/models.py
+++ allactions/models.py
@@ -1,31 +1,40 @@
 from django.db import models
 
+
 class Department(models.Model):
     class Meta:
         db_table = "department"
+
     name = models.CharField(max_length=35)
+
 
 class Section(models.Model):
     class Meta:
         db_table = "section"
+
     name = models.CharField(max_length=35)
+
 
 class Question(models.Model):
     class Meta:
         db_table = "question"
+
     title = models.CharField(max_length=35)
     fStage = models.ForeignKey("Stage", default=None, on_delete=models.DO_NOTHING)
     department = models.ManyToManyField(Department)
     hint = models.TextField(max_length=300, null=True, blank=True, default=None)
 
+
 class Grade(models.Model):
     class Meta:
         db_table = "grade"
+
     name = models.CharField(max_length=35)
 
 
 class Stage(models.Model):
     class Meta:
         db_table = "stage"
+
     title = models.CharField(max_length=35)
     fSection = models.ForeignKey('Section', default=None, on_delete=models.DO_NOTHING)
--- my_site/views.py
+++ my_site/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+# from django.http import HttpResponse
 
+from allactions.models import Stage
+from allactions.models import Section
 
-def index(request):
-    return render(request, 'index.html')
+def base(request):
+    # sections = Section.objects.all()
+    stages = Stage.objects.all()
+
+    return render(request, 'stages.html', context={'stages': stages})
+
+# def stage(request):
+#     stages = Stage.objects.all()
+#     return render(request)
+#

55c3038216d98ab137c43dccd1be51a5ed68351c
Oleg Sliusar <olehsiusar@gmail.com>
2019-02-07 19:39:27 +0200

    Make almost finish

--- my_site/views.py
+++ my_site/views.py
@@ -1,19 +1,28 @@
 from django.shortcuts import render
-# from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
 from allactions.models import *
 
-def base(request, id_stg):
-    sections = Section.objects.all()
-    stages = Stage.objects.filter(fSection=sections[0].id)
-    stage = Stage.objects.get(pk=id_stg)
-    questions = Question.objects.filter(fStage_id=stage.id)
-    all_questions = Question.objects.all()
-    next_stage = Stage.objects.get(pk=id_stg+1)
-    context = {'stages': stages, 'questions': questions, 'sections': sections, 'stage': stage, 'all_questions': all_questions, 'next_stage':next_stage }
-    return render(request, 'base.html', context=context)
 
-# def stage(request):
-#     stages = Stage.objects.all()
-#     return render(request)
-#
+@login_required(login_url='')
+def base(request, id_stg=1):
+        sections = Section.objects.all()
+        stages = Stage.objects.all()
+        stage_sk = Stage.objects.filter(fSection=1).first()
+        stage_act = Stage.objects.filter(fSection=2).first()
+        stage = Stage.objects.get(pk=id_stg)
+        questions = Question.objects.filter(fStage_id=stage.id)
+        all_questions = Question.objects.all()
+
+        if(id_stg>1 and id_stg<len(stages)):
+                next_stage = Stage.objects.get(pk=id_stg+1)
+                prev_stage = Stage.objects.get(pk=id_stg-1)
+        else:
+                next_stage = Stage.objects.get(pk=2)
+                prev_stage = Stage.objects.get(pk=1)
+
+        context = {'stages': stages, 'questions': questions, 'sections': sections, 'stage': stage,
+                   'all_questions': all_questions, 'next_stage': next_stage, 'prev_stage': prev_stage,
+                   'stage_sk': stage_sk, 'stage_act': stage_act}
+        return render(request, 'base.html', context=context)
+
